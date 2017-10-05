36d09600c6fb6afafc5ff85200659f4a44481c83
minemichi <e155736@ie.u-ryukyu.ac.jp>
2017-10-05 19:01:05 +0900

    add edit-top

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def top
   end
+  def introduct
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/top'
+  # defaultページを固定
+  get '/' => "home#top"
+  get 'home/introduct' => "home#introduct"
+  # 定型文
+  # get '' => ""
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
