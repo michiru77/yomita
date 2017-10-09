39418e9cd69419d34f291f1d7384fd02af64778f
Sunagawa <e155746@ie.u-ryukyu.ac.jp>
2017-10-09 15:04:58 +0900

    modify URL

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
+# coding: utf-8
 Rails.application.routes.draw do
   # defaultページを固定
-  get '/' => "home#top"
+  get 'home/top' => "home#top"
   get 'home/introduct' => "home#introduct"
   get 'home/minemichi' => "home#e155736"
-  get 'home/sandbox' => "home#sandbox"
+  get '/' => "home#sandbox"
   # 定型文
   # get '' => ""
 
