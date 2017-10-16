18a2b3e899bb3e658bcfcf96ef33829f719a585b
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2017-10-16 15:46:42 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'home#sandbox'
-  get 'index/frontcover' => "index#frontcover"
+  get 'index/frontcover'
   get '/img1' => "minemichi#img1"
   get '/upsideDown1' => "minemichi#upsideDown1"
 end
