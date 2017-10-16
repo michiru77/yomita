a403f85d836af089ef334d589a0dd83cdbd11fca
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2017-10-16 16:33:23 +0900

    Update michiru_panel_img_controller

--- test/controllers/michiru_panel_img_controller_test.rb
+++ test/controllers/michiru_panel_img_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class MichiruPanelImgControllerTest < ActionDispatch::IntegrationTest
   test "should get img1" do
-    get michiru_panel_img_img1_url
+    get img1_url
     assert_response :success
   end
 
