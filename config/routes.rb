a5fdaf9e48821b18273069f83ed46347ac82a0b5
Sunagawa <e155746@ie.u-ryukyu.ac.jp>
2017-10-17 15:57:19 +0900

    modify route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'home#sandbox'
-  get '/frontcover' => 'index#frontcover'
+  #root 'home#sandfbox'
+  get '/' => 'index#frontcover'
+  get '/sandbox' => 'home#sandbox'
   get '/upsideDown1' => "michiru_panel_upside_down#upsideDown1"
   get '/img1' => "michiru_panel_img#img1"
 end
