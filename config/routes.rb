c2940fbad8ae7df7a6aaad91b16df23bdb7f9a1e
minemichi <e155736@ie.u-ryukyu.ac.jp>
2017-10-17 15:46:58 +0900

    edit img1 and upsideDown1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'home#sandbox'
+  #root 'home#sandbox'
+  root 'michiru_panel_img#img1'
   get '/frontcover' => 'index#frontcover'
   get '/upsideDown1' => "michiru_panel_upside_down#upsideDown1"
   get '/img1' => "michiru_panel_img#img1"
