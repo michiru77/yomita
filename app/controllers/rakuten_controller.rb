024a2e86b3d6e981129d3704f5d869d54cb342f4
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2017-11-09 13:38:22 +0900

    tmp

--- app/controllers/rakuten_controller.rb
+++ app/controllers/rakuten_controller.rb
@@ -1,9 +1,9 @@
 class RakutenController < ApplicationController
   def search
     if params[:keyword]
-      @items1 = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword], hits: 1)
-      @items2 = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword], hits: 3)
-      @items3 = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword], hits: 30)
+      @items1 = RakutenWebService::Books::Book.search(keyword: params[:keyword], hits: 1)
+      @items2 = RakutenWebService::Books::Book.search(keyword: params[:keyword], hits: 3)
+      @items3 = RakutenWebService::Books::Book.search(keyword: params[:keyword], hits: 30)
     end
   end
 end
