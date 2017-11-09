21f83721c046a0accb2d7f70c5c8129439f2e7e6
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2017-11-09 14:09:49 +0900

    Modify controller and view

--- app/controllers/rakuten_controller.rb
+++ app/controllers/rakuten_controller.rb
@@ -1,6 +1,8 @@
+# coding: utf-8
 class RakutenController < ApplicationController
   def search
     if params[:keyword]
+
       @items1 = RakutenWebService::Books::Book.search(keyword: params[:keyword], hits: 1)
       @items2 = RakutenWebService::Books::Book.search(keyword: params[:keyword], hits: 3)
       @items3 = RakutenWebService::Books::Book.search(keyword: params[:keyword], hits: 30)
