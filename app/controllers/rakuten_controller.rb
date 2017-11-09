99efc9138dd3b9b843936ea0b8b85348557b4ea9
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2017-11-09 17:01:05 +0900

    Change search mode keyword to author

--- app/controllers/rakuten_controller.rb
+++ app/controllers/rakuten_controller.rb
@@ -1,11 +1,11 @@
 # coding: utf-8
 class RakutenController < ApplicationController
   def search
-    if params[:keyword]
+    if params[:title]
 
-      @items1 = RakutenWebService::Books::Book.search(keyword: params[:keyword], hits: 1)
-      @items2 = RakutenWebService::Books::Book.search(keyword: params[:keyword], hits: 3)
-      @items3 = RakutenWebService::Books::Book.search(keyword: params[:keyword], hits: 30)
+      @items1 = RakutenWebService::Books::Book.search(title: params[:title], hits: 1)
+      @items2 = RakutenWebService::Books::Book.search(title: params[:title], hits: 3)
+      @items3 = RakutenWebService::Books::Book.search(title: params[:title], hits: 30)
     end
   end
 end
