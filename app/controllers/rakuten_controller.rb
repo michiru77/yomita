498f845aaf16aca3e43eb7288fcebce41a5140b5
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2017-11-10 17:09:37 +0900

    Modify Gemfile

--- app/controllers/rakuten_controller.rb
+++ app/controllers/rakuten_controller.rb
@@ -4,6 +4,7 @@ class RakutenController < ApplicationController
     if params[:title]
 
       @items1 = RakutenWebService::Books::Book.search(title: params[:title], hits: 1)
+      binding.pry
       @items2 = RakutenWebService::Books::Book.search(title: params[:title], hits: 3)
       @items3 = RakutenWebService::Books::Book.search(title: params[:title], hits: 30)
     end
