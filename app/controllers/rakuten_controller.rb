edbf3c29bde571b856844ad298c6f4b38a7d0094
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2017-11-10 16:45:43 +0900

    Modify rakuten_controller

--- app/controllers/rakuten_controller.rb
+++ app/controllers/rakuten_controller.rb
@@ -1,10 +1,12 @@
-# coding: utf-8
 class RakutenController < ApplicationController
   def search
     if params[:title]
-
-      @items1 = RakutenWebService::Books::Book.search(title: params[:title], hits: 1)
-      @items2 = RakutenWebService::Books::Book.search(title: params[:title], hits: 3)
+      #      @items1 = RakutenWebService::Books::Book.search(title: params[:title], hits: 1)
+      #      @items2 = RakutenWebService::Books::Book.search(title: params[:title], hits: 3)
+      #      @items3 = RakutenWebService::Books::Book.search(title: params[:title], hits: 30)
+      @list = RakutenWebService::Books::Book.search(title: params[:title], hits: 30)
+      @items1 = @list[0]
+      @items2 = RakutenWebService::Books::Book.search(title: params[:title], hits: 30)
       @items3 = RakutenWebService::Books::Book.search(title: params[:title], hits: 30)
     end
   end
