91ed3ad5140489a37c26887281c21e1cb8975fbc
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2017-11-13 00:15:31 +0900

    Refactoring

--- app/controllers/rakuten_controller.rb
+++ app/controllers/rakuten_controller.rb
@@ -2,11 +2,16 @@
 class RakutenController < ApplicationController
   def search
     if params[:title]
+      @items = RakutenWebService::Books::Book.search(title: params[:title], hits: 30)
 
-      @items1 = RakutenWebService::Books::Book.search(title: params[:title], hits: 1)
-      binding.pry
-      @items2 = RakutenWebService::Books::Book.search(title: params[:title], hits: 3)
-      @items3 = RakutenWebService::Books::Book.search(title: params[:title], hits: 30)
+      @items_1 = []
+      @items_2 = []
+      @items_3 = []
+      @items.each_with_index do |item, i|
+        @items_1 << item if i == 0
+        @items_2 << item if (i >= 1 && i <= 3)
+        @items_3 << item if i >= 4
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # root 'index#frontcover'
+  #root 'index#frontcover'
   root 'rakuten#search'
   get 'rakuten_search' => 'rakuten#search'
 end
