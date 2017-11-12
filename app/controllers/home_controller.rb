c4359fcabdd71aab359665c2a5bc9877157d6103
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2017-11-13 03:31:05 +0900

    Add index.html.erb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,22 @@
 class HomeController < ApplicationController
+  def index
+  end
+
+=begin
+  def search
+    if params[:title]
+      @items = RakutenWebService::Books::Book.search(title: params[:title], hits: 30)
+
+      @items_1 = []
+      @items_2 = []
+      @items_3 = []
+      @items.each_with_index do |item, i|
+        @items_1 << item if i == 0
+        @items_2 << item if (i >= 1 && i <= 3)
+        @items_3 << item if i >= 4
+      end
+    end
+  end
+=end
+
 end
