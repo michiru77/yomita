4c94ba97bcee6dfdb5656520957439362bd176f0
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2017-11-14 19:17:41 +0900

    Modifying

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -1,130 +1,8 @@
-//= require rails-ujs
-//= require jquery
-//= require turbolinks
-//= require_tree .
-//= require rails-ujs
-//= require jquery
-//= require turbolinks
-//= require_tree .
-
-
-/*
-$(function(){
-    var h = '<div class="inner_b">'
-        + 'コンテンツB'
-        + '</div>'
-        + '<div class="inner_c">'
-        + 'コンテンツC'
-        + '</div>';
-    $("#wrap").append( h );
-});
-*/
-
-
-
-
-/*
-$(function(){
-    $.ajax({
-        url: "/home/search",
-        type: "POST",
-        data: {title:"化学"},
-        datatype: "json",
-        success: function(data){
-            var h = '<div class="inner_b">'
-                + 'コンテンツB'
-                + '</div>'
-                + '<div class="inner_c">'
-                + 'コンテンツC'
-                + '</div>';
-            $("#wrap").append( h );
-            //$("#add").append(item.large_image_url);
-        },
-        error: function(data){
-            //失敗時の処理
-        }
-    });
-});
-*/
-
-/*
-function ajaxSearch(keyword,page) {
-    if (!keyword) {
-        keyword = $('.submit.active').attr('value');
-    }
-    $.ajax({
-        type: 'GET',
-        url: 'https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404',
-        data: {
-            applicationId: '1011423156545588403', // 必須
-            affiliateId: '1619c75a.e3347e97.1619c75b.0859ce2c',
-            keyword: keyword,
-            page: page
-        }
-    }).done(function (data) {
-        _getItems(data)
-    })
-}
-
-function _getItems(data) {
-    //console.log(data);
-    $('#container').empty();
-    var pageCount = data.pageCount;
-    var current = data.page;
-
-    var dataStat = data.count;
-    if (dataStat > 0) {
-        $.each(data.Items, function (i, items) {
-            var item = items.Item;
-            var affiliateUrl = item.affiliateUrl;
-            var imageUrl = item.mediumImageUrls[0].imageUrl;
-            var itemName = item.itemName;
-            if (itemName.length > 10) {
-                itemName = itemName.substring(0, 10) + '...';
-            }
-            var itemPrice = item.itemPrice;
-            var htmlTemplate = $('<div class="grid">' +
-                '<div class="imgholder swing">' +
-                '<a href="' + affiliateUrl + '">' +
-                '<img src="' + imageUrl + '" alt="' + item.itemName + '" width="128" ' +
-                'height="128"/>' +
-                '</a></div>' +
-                '<h2><a href="' + affiliateUrl + '">' + itemName + '</a></h2>' +
-                '<p>' + item.itemName + '</p>' +
-                '<div class="meta">' + comma(itemPrice) + '円</div>' +
-                '</div>');
-
-            //テンプレートを追加
-            $('#container').append(htmlTemplate);
-
-        });
-    });//each
-}
-
-$(function _action() {
-    $('.submit').click(function () {
-        var keyword = $(this).attr('value');
-        //var oElements = document.getElementById("#button1");
-        //var keyword = oElements.value();
-        if (!keyword) {
-            return;
-        }
-        ajaxSearch(keyword);
-    })
-});
-
-
-$(document).ready(function() {
-    $("p").css("color", "blue");
-});
-*/
-
-
 $(function() {
     $(window).load(function() {
-        $("title").on("click", function(){
-            var keyword = $("#title").val(); 
-            //var keyword = $(this).attr('value');
+        $('.submit').on("click", function(){
+            // var keyword = $("#title").val();
+            var keyword = $(this).attr('value');
             if (!keyword) {
                 return;
             }
@@ -145,7 +23,7 @@ function ajaxSearch(keyword,page) {
             applicationId: '1011423156545588403', // 必須
             affiliateId: '1619c75a.e3347e97.1619c75b.0859ce2c',
             title: keyword,
-//            booksGenreId: keyword,
+            //            booksGenreId: keyword,
             hits: 10,
             itemCaption: keyword,
             //page: 10,
@@ -157,8 +35,6 @@ function ajaxSearch(keyword,page) {
         _getItems(data)
     })
 }
-
-
 )
 function _getItems(data) {
     //$('.seikou').hide();
@@ -198,7 +74,6 @@ function _getItems(data) {
 
     }
 }
-
 
 
 /*
