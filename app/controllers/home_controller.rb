ccb1b3a4110e87b605671d8c65f537f067d8f30c
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2017-11-17 21:19:53 +0900

    Modifying

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,9 +6,8 @@ class HomeController < ApplicationController
 
   def search
     data = RakutenWebService::Books::Book.search(title: params[:title], hits: 2)
-    # render :action => "index"
     render :json => data
-    #    binding.pry
+#    binding.pry
   end
 
 end
