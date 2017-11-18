57927f0b829e744c6372ca2df2de5ea4bd8b839d
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2017-11-18 17:21:23 +0900

    Refactoring my_books.js

--- app/assets/javascripts/my_books.js
+++ app/assets/javascripts/my_books.js
@@ -3,27 +3,33 @@
 /*
   $(window).load(function() {
   $('.submit',).on('click',function() {
-  var t = 'hoge'
-  var hoge = '<div>'
-  + '<p>'
-  + t
-  + '</p>'
-  + '</div>';
-  $('#wrap').append(hoge);
   });
   });
 */
 
 $(window).load(function(){
+    var author = '西尾維新';
     var title = '恋愛';
-    var hits = 3;
-    //    searchBooks(title, hits);
-    tt(title, hits);
+    var hits = 30;
+    setBooks(author, hits);
 });
 
-function searchBooks(title, hits) {
+function authorSearch(author, hits) {
     return $.ajax({
-        url: '/home_search',
+        url: '/home_authorSearch',
+        type: 'GET',
+        dataType: 'json',
+        async: true,
+        data: {
+            author: author,
+            hits: hits
+        }
+    });
+}
+
+function titleSearch(title, hits) {
+    return $.ajax({
+        url: '/home_titleSearch',
         type: 'GET',
         dataType: 'json',
         async: true,
@@ -34,71 +40,36 @@ function searchBooks(title, hits) {
     });
 }
 
-function tt(title, hits) {
-    searchBooks(title, hits).done(function(data){
-        var list;
-        $.each(data, function (i) {
-            var url = data[i]["params"]["itemUrl"];
-            var imgUrl = data[i]["params"]["largeImageUrl"];
-            list = '<p><img src="'
-                + imgUrl
-                +'"></p>';
-            $("#photos_6").append(list);
-        });
+function setBooks(author, hits) {
+    authorSearch(author, hits).done(function(data){
+        getBooks(data);
     }).fail(function(data){
         $('#out').html('<p>Failure</p>');
     });
 }
 
-
-
 function getBooks(data) {
-    var list;
     $.each(data, function (i) {
         var url = data[i]["params"]["itemUrl"];
         var imgUrl = data[i]["params"]["largeImageUrl"];
-        list = '<p><img src="'
+        var list = '<p><img src="'
             + imgUrl
             +'"></p>';
         $("#photos_6").append(list);
     });
 }
 
-function _getItems(data) {
-    //$('.seikou').hide();
-    // console.log(data);
-    $('#container').empty();
-    //    var pageCount = data.pageCount;
-    //    var current = data.page;
 
-    var dataStat = data.count;
-    if (dataStat > 0) {
-        $.each(data.Items, function (i, items) {
-            var item = items.Item;
-            var affiliateUrl = item.affiliateUrl;
-            var largeimageUrl = item.largeImageUrl;
-            var itemCap = item.itemCaption;
-            //var mediumimageUrl = item.mediumImageUrl;
-
-            var itemName = item.title;
-            if (itemName.length > 10) {
-                itemName = itemName.substring(0, 10) + '...';
-            }
-            var itemPrice = item.itemPrice;
-
-            if (largeimageUrl !='https://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/noimage_01.gif?_ex=200x200'){
-                var htmlTemplate = $('<div class="grid">' +
-                                     '<div class="imgholder swing">' +
-                                     '<a href="' + affiliateUrl + '">' +
-                                     '<img src="' + largeimageUrl + '" alt="' + item.itemName + '" width="200" ' +
-                                     'height="200"/>' +
-                                     '</a></div>'+'<p>'+ itemCap + '</p>' );
-            }
-
-            //テンプレートを追加
-            $('#container').append(htmlTemplate);
-
-        });
-
-    }
-}
+/*
+  function getBooks(data) {
+  var list;
+  $.each(data, function (i) {
+  var url = data[i]["params"]["itemUrl"];
+  var imgUrl = data[i]["params"]["largeImageUrl"];
+  list = '<p><img src="'
+  + imgUrl
+  +'"></p>';
+  $("#photos_6").append(list);
+  });
+  }
+*/
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,16 @@ class HomeController < ApplicationController
   def index
   end
 
-  def search
+  def authorSearch
+    data = RakutenWebService::Books::Book.search(author: params[:author],
+                                                 hits: params[:hits])
+    render :json => data
+    #binding.pry
+  end
+
+  def titleSearch
     data = RakutenWebService::Books::Book.search(title: params[:title],
                                                  hits: params[:hits])
-
     render :json => data
     #binding.pry
   end
