d8fb21cba613bbe423840ce745e4753feb5e1dee
minemichi <e155736@ie.u-ryukyu.ac.jp>
2017-11-21 14:58:41 +0900

    add create-cookie

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,7 @@
 class IndexController < ApplicationController
   def frontcover
   	gon.historyList = session[:history]
+  	@location = request.url
   	@reset = params[:reset]
   	@bookID = params[:bookID]
   	@historyList = session[:history]
