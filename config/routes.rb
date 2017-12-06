38315b1f461564926a28d24ef75a5d2ff9da6d2c
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2017-12-06 18:00:43 +0900

    [clean] Delete routing config for index controlle

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  # root 'index#frontcover'
   # root 'rakuten#search'
   # get 'rakuten_search' => 'rakuten#search'
-  # get '/historypage' => 'index#historypage'
+  root 'home#index'
   get 'home_history' => 'home#history'
   get 'home_index' => 'home#index'
-  root 'home#index'
   get 'home_authorSearch' => 'home#authorSearch'
   get 'home_titleSearch' => 'home#titleSearch'
   get 'home_genreSearch' => 'home#genreSearch'
