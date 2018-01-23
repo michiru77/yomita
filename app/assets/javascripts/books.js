c8e7841d640033a436b78787111090070783eecc
Sunagawa <e155746@ie.u-ryukyu.ac.jp>
2018-01-23 10:52:43 +0900

    add sortSearch x 2

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -34,7 +34,14 @@ $(window).load(function(){
     sleep(1000);
     sortSearch(sort,pg.getRandPage(),1);
 
+    sleep(1000);
+    sortSearch(sort,pg.getRandPage(),1);
 
+    sleep(1000);
+    sortSearch(sort,pg.getRandPage(),1);
+
+    //sleep(1000);
+    //sortSearch(sort,pg.getRandPage(),1);
 
     // appendList[]の初期化
     appendList = new Array();
